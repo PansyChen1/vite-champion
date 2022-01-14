7a590e4552627be8411fdb12476268f869d56473
pingchen <chenping_champion@163.com>
2022-01-14 13:28:46 +0800

    feat: basic config

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // resolve: {
+  //   alias: {
+  //     '@': resolve(__dirname, 'src')
+  //   }
+  // },
+  base: './', // 设置打包路径
+  server: {
+    port: 4000, // 设置服务启动端口号
+    open: true,
+    cors: true
+  }
+})
